388d84d0f379232e1be101e2c29852766e813c99
manuel_alfaro_sierra <manuel.alfaro.sierra@gmail.com>
2016-10-16 15:29:31 +0200

    Adaptation to new model with modules

--- mconv.js
+++ mconv.js
@@ -2,43 +2,55 @@
 
 const fs = require('fs');
 const path = require('path');
+const modprefix = 'mconv_mod';
 
 class Mconv {
     constructor(options) {
         this.converter = null;
+        this.mods = [];
     }
 
     _help() {
         console.log('Use: mconv [type] [modificators]');
-        let folders = fs.readdirSync('./modules');
-        console.log('Type of media conversions:');
-        for (let i = 0; i < folders.length; i++) {
-            console.log('----------------------------------------------------------');
-            console.log(folders[i]);
-            console.log('Use: mconv %s -help', folders[i]);
-            console.log('----------------------------------------------------------');
+        console.log('Sample: mconv flac2mp3 -help');
+        let dependencies = require(path.join(__dirname, 'package.json')).dependencies;
+
+        for (let dep in dependencies) {
+            if (dep.indexOf(modprefix) !== -1) {
+                this.mods.push(dep.substr(modprefix.length + 1));
+            }
         }
+
+        if (this.mods.length > 0) {
+            console.log('Type of media conversions:');
+            for (let i = 0; i < this.mods.length; i++) {
+                console.log('-%s', this.mods[i]);
+            }
+        }
+
+        return false;
     }
 
     _execModule(module) {
         try {
-            const Converter = require(path.join(__dirname + '/modules', process.argv[2], 'converter.js'));
+            //const Converter = require(path.join(__dirname + '/node_modules/mconv_mod_' + process.argv[2], 'mconv_mod_' + process.argv[2]));
+            const Converter = require(modprefix + '_' + process.argv[2]);
+            //const Converter = require('mconv_mod_flac2mp3');
             this.converter = new Converter();
             this.converter[module]();
-            return false;
         } catch (error) {
             console.error('[MCONV] Module %s not found', process.argv[2]);
             console.error('[MCONV] ' + error);
-            return false;
         }
+        return false;
     }
 
     _validation() {
+        //console.log(process.argv);
         if (process.argv.length === 2) {
-            this._help();
-            return false;
-        } else if (process.argv.length === 4 && process.argv.indexOf('-help') !== -1) {
-            this._execModule('_help');
+            return this._help();
+        } else if (process.argv.length === 5 && process.argv.indexOf('-help') !== -1) {
+            return this._execModule('_help');
         }
 
         return true;
--- modules/flac2mp3/templates/flac2mp3.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -i "<%= filePath %>" -f mp3 -ab 192000 "<%= folderPath %><%= fileName %>.<%= fileExtTo %>" 2> /dev/null
